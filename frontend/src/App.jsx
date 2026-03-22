3d9a1b7a2104a6119b5fe50f764b2655a0a661b4
kapil-bhargava <kapilkkb28@gmail.com>
2026-03-22 20:59:38 +0530

    coo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,10 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateRoom from "./pages/CreateRoom";
 import JoinRoom from "./pages/Joinroom";
-import ChatRoom from "./pages/ChatRoom";
+
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import ChatRoom from "./pages/ChatRoom";
 
 
 function App() {
